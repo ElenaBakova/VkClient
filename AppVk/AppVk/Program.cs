cdd8e5f1ba37406920f67ab51de92dccdc43dbf1
Elena Bakova <lenabakova72@gmail.com>
2021-10-26 16:32:42 +0300

    a

--- AppVk/AppVk/Program.cs
+++ AppVk/AppVk/Program.cs
@@ -16,19 +16,20 @@ namespace AppVk
     class Program
     {
         
-        static async Task<string> UserInfo(string token, string userId)
+        static async Task<string> UserInfo(string userId)
         {
-            string uri = $"https://api.vk.com/method/users.get?user_ids={userId}&access_token={token}&v=5.131";
+            var token = "44fcc8d244fcc8d244fcc8d2e944851f70444fc44fcc8d2258b3199825618b6202a56f8";
+            string uri = $"https://api.vk.com/method/users.get?user_ids={userId}&access_token={token}&v=5.131&fields=can_write_private_message";
             var httpClient = new HttpClient();
-            var request = new HttpRequestMessage(HttpMethod.Get, uri);
             return await httpClient.GetStringAsync(uri);
         }
 
         private static async Task Main(string[] args)
         {
-            string token = "44fcc8d244fcc8d244fcc8d2e944851f70444fc44fcc8d2258b3199825618b6202a56f8";
-            string userId = "560243831";
-            var json = await UserInfo(token, userId);
+            Console.WriteLine("Please enter userId");
+            var userId = Console.ReadLine();
+            //string userId = "560243831";
+            var json = await UserInfo(userId);
             
             Console.WriteLine(json);
         }
